79b6954f164d187a409e242341bb76f4d05357dd
nobii <fujisawa-shin@kayac.com>
2019-07-14 20:33:10 +0900

    lint on saveしてくぞ

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   exportTrailingSlash: true,
-}
+  webpack: config => {
+    // lint on save
+    config.module.rules.push({
+      test: /\.(js|ts|tsx|$)/,
+      loader: "eslint-loader",
+      enforce: "pre",
+      options: {
+        fix: true
+      }
+    });
+    return config;
+  }
+};
