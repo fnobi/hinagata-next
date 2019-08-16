c99d0d8a81c16084515d3762e6661fbd81e5583e
nobii <fujisawa-shin@kayac.com>
2019-08-16 12:57:18 +0900

    この記述は必要

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,3 +1,3 @@
-// / <reference types="next" />
-// / <reference types="next/types/global" />
+/// <reference types="next" />
+/// <reference types="next/types/global" />
 declare module 'emotion-reset';
