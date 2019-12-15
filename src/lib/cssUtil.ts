c20075d9d269493dd317b45b9342b64431cc5c7d
nobii <fujisawa-shin@kayac.com>
2019-12-16 01:50:17 +0900

    lint.

--- src/lib/cssUtil.ts
+++ src/lib/cssUtil.ts
@@ -1,15 +1,15 @@
 const appendPostfix = (nums: number[], postfix: string) => {
   return nums.map<string>(n => `${n}${postfix}`).join(" ");
-}
+};
 
 export const px = (...nums: number[]) => {
-  return appendPostfix(nums, 'px');
+  return appendPostfix(nums, "px");
 };
 
 export const percent = (...nums: number[]) => {
-  return appendPostfix(nums, '%');
+  return appendPostfix(nums, "%");
 };
 
 export const em = (...nums: number[]) => {
-  return appendPostfix(nums, 'em');
+  return appendPostfix(nums, "em");
 };
