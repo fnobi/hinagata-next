f01c44626811d5a7db782b482c1c9174e79b7666
nobii <fujisawa-shin@kayac.com>
2020-07-13 22:12:41 +0900

    meta => layoutの繋ぎこみ周り整理

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,37 +1,45 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import Head from "next/head";
 import emotionReset from "emotion-reset";
 import { Global, css } from "@emotion/core";
 import { globalStyle } from "~/local/commonCss";
 import META from "~/local/meta";
 
-type Props = {
+const DefaultLayout = (props: {
   title?: string;
+  shareImage?: string;
+  children: ReactNode;
+}) => {
+  const { children, title: pageTitle, shareImage: pageShareImage } = props;
+  const title = pageTitle ? `${pageTitle} | ${META.TITLE}` : META.TITLE;
+  const shareImageUrl = `${URL}${pageShareImage || META.SHARE_IMAGE_PATH}`;
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="viewport" content="width=device-width initial-scale=1" />
+        <meta name="description" content={META.DESCRIPTION} />
+        {META.KEYWORDS ? (
+          <meta name="keywords" content={META.KEYWORDS.join(",")} />
+        ) : null}
+        <meta property="og:url" content={META.URL} />
+        <meta property="og:image" content={shareImageUrl} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={META.DESCRIPTION} />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:image" content={shareImageUrl} />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={META.DESCRIPTION} />
+        <link rel="canonical" href={META.URL} />
+        {META.FAVICON_PATH ? (
+          <link rel="icon" type="image/x-icon" href={META.FAVICON_PATH} />
+        ) : null}
+      </Head>
+      <Global styles={css(emotionReset, globalStyle)} />
+      {children}
+      {/* <GTagSnippet trackingId="XX-XXXXXXXXX-XX" /> */}
+    </>
+  );
 };
 
-export default (({ children, title }) => (
-  <div>
-    <Head>
-      <title>
-        {title ? `${title} | ` : ""}
-        {META.TITLE}
-      </title>
-      <meta name="viewport" content="width=device-width initial-scale=1" />
-      <meta name="description" content={META.DESCRIPTION} />
-      <meta name="keywords" content={META.KEYWORDS.join(",")} />
-      <meta property="og:url" content={META.URL} />
-      <meta property="og:image" content={META.SHARE_IMAGE_URL} />
-      <meta property="og:title" content={META.TITLE} />
-      <meta property="og:description" content={META.DESCRIPTION} />
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:image" content={META.SHARE_IMAGE_URL} />
-      <meta property="twitter:title" content={META.TITLE} />
-      <meta property="twitter:description" content={META.DESCRIPTION} />
-      <link rel="canonical" href={META.URL} />
-      <link rel="icon" type="image/x-icon" href={META.FAVICON_PATH} />
-    </Head>
-    <Global styles={css(emotionReset, globalStyle)} />
-    {children}
-    {/* <GTagSnippet trackingId="XX-XXXXXXXXX-XX" /> */}
-  </div>
-)) as React.FunctionComponent<Props>;
+export default DefaultLayout;
--- src/local/meta.ts
+++ src/local/meta.ts
@@ -3,27 +3,20 @@ import ASSETS_FAVICON from "~/assets/meta/favicon.ico";
 
 const { basePath } = require("../../buildConfig");
 
-export type Meta = {
+type Meta = {
   TITLE: string;
   DESCRIPTION: string;
   URL: string;
-  KEYWORDS: string[];
-  SHARE_IMAGE_URL: string;
-  FAVICON_PATH: string;
+  KEYWORDS?: string[];
+  SHARE_IMAGE_PATH: string;
+  FAVICON_PATH?: string;
 };
 
-const TITLE = "hinagata-next";
-const DESCRIPTION = "Awsome next.js project.";
-const KEYWORDS = ["next", "javascript"];
-const URL = `${process.env.SITE_ORIGIN}${basePath}`;
-const SHARE_IMAGE_URL = `${URL}${ASSETS_OGP}`;
-const FAVICON_PATH = ASSETS_FAVICON;
-
 export default {
-  TITLE,
-  DESCRIPTION,
-  KEYWORDS,
-  URL,
-  SHARE_IMAGE_URL,
-  FAVICON_PATH
+  TITLE: "hinagata-next",
+  DESCRIPTION: "Awsome next.js project.",
+  KEYWORDS: ["next", "javascript"],
+  URL: `${process.env.SITE_ORIGIN}${basePath}`,
+  SHARE_IMAGE_PATH: ASSETS_OGP,
+  FAVICON_PATH: ASSETS_FAVICON
 } as Meta;
