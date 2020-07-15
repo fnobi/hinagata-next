a87287fcbcf809617d140b4032132a101990dd19
nobii <fujisawa-shin@kayac.com>
2020-07-15 10:06:28 +0900

    dist作成の手順なおした

--- scripts/make-dist-dir.js
+++ scripts/make-dist-dir.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const fs = require("fs");
-const mkdirp = require("mkdirp");
 const { basePath } = require("../buildConfig");
 
 const EXPORT_DIR = "out";
@@ -13,12 +12,12 @@ function main() {
     throw new Error(`"${EXPORT_DIR}" doesn't exist.`);
   }
   if (fs.existsSync(distPath)) {
-    throw new Error(`"${distPath}" exists.`);
+    fs.rmdirSync(distPath, { recursive: true });
+  } else {
+    fs.mkdirSync(distPath, { recursive: true });
+    fs.rmdirSync(distPath);
   }
-  return Promise.resolve()
-    .then(() => mkdirp(distPath))
-    .then(() => fs.rmdirSync(distPath))
-    .then(() => fs.renameSync(EXPORT_DIR, distPath));
+  fs.renameSync(EXPORT_DIR, distPath);
 }
 
 main();
