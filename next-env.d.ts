c3f86b1f3d05394cc877232681639be66494c4db
fnobi <fnobi0.8@gmail.com>
2021-07-29 19:02:08 +0900

    いらんやった

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-/// <reference types="next-images" />
 declare module "emotion-reset";
 declare module "*.mp4";
 declare module "*.mp3";
