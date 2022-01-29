9905e60ffd4257be454715a080999ac487fdfe62
fnobi <fnobi0.8@gmail.com>
2022-01-29 15:02:38 +0900

    file-loaderまわりの設定

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
+const BASE_PATH = "";
+
+const ENV = {
+  SITE_ORIGIN: process.env.SITE_ORIGIN || "https://example.com"
+};
+
 module.exports = {
-  basePath: "",
-  env: {
-    SITE_ORIGIN: process.env.SITE_ORIGIN || "https://example.com"
-  },
+  basePath: BASE_PATH,
+  env: ENV,
   trailingSlash: true,
+  webpack: config => {
+    config.module.rules.push({
+      loader: "file-loader",
+      test: /\.(mp4|mp3)$/i,
+      options: {
+        publicPath: `${BASE_PATH}/_next/static/files`,
+        outputPath: "static/files"
+      },
+    });
+    return config;
+  }
 };
---
+++ typings/_file-loader.d.ts
@@ -0,0 +1,8 @@
+declare module "*.mp4" {
+  const mp4: string;
+  export default mp4;
+}
+declare module "*.mp3" {
+  const mp3: string;
+  export default mp3;
+}
