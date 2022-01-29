fb6c46db251ccdfaa9485d7b4bfa123840477187
Fujisawa Shin <fnobi0.8@gmail.com>
2022-01-29 15:30:53 +0900

    書き方のアップデート

--- pages/pixi.tsx
+++ pages/pixi.tsx
@@ -1,10 +1,10 @@
-import React from "react";
 import dynamic from "next/dynamic";
+import { NextPage } from "next";
 
 const SamplePixiView = dynamic(() => import("~/components/SamplePixiView"), {
   ssr: false
 });
 
-const PagePixi = () => <SamplePixiView />;
+const PagePixi: NextPage = () => <SamplePixiView />;
 
 export default PagePixi;
