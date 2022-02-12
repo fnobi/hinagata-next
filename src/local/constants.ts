42581f8f1d57394f646330851d974b783f099b11
Fujisawa Shin <fnobi0.8@gmail.com>
2022-02-12 15:43:21 +0900

    環境変数の読み方見直し

--- src/local/constants.ts
+++ src/local/constants.ts
@@ -2,5 +2,6 @@ const config = require("../../next.config");
 
 const { basePath } = config;
 
-export const { SITE_ORIGIN } = process.env;
 export const BASE_PATH = basePath;
+export const SITE_ORIGIN =
+  process.env.NEXT_PUBLIC_SITE_ORIGIN || "https://example.com";
