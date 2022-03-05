0bf842e815e83effa78b3007cf20da0ba33bf1b9
Fujisawa Shin <fnobi0.8@gmail.com>
2022-03-05 14:35:26 +0900

    セクション情報を別で渡す

--- src/lib/useFormLogic.ts
+++ src/lib/useFormLogic.ts
@@ -1,12 +1,21 @@
 import { useState } from "react";
+import { every } from "~/lib/arrayUtil";
 
-function useFormLogic<T>(opts: { defaultValue: T }) {
-  const { defaultValue } = opts;
+export type FormPlot<T> = {
+  title: string;
+  required: boolean;
+  key: keyof T;
+};
+
+function useFormLogic<T>(opts: { defaultValue: T; plot: FormPlot<T>[] }) {
+  const { defaultValue, plot } = opts;
   const [current, setCurrent] = useState(defaultValue);
   function updateForm<TT extends keyof T>(key: TT, val: T[TT]) {
     return setCurrent(c => ({ ...c, [key]: val }));
   }
-  return { current, updateForm };
+  const sections = plot.map(p => ({ plot: p, valid: !!current[p.key] }));
+  const valid = every(sections, s => s.valid);
+  return { current, updateForm, sections, valid };
 }
 
 export default useFormLogic;
--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -1,35 +1,72 @@
 import { NextPage } from "next";
+import { FormEvent } from "react";
 import useFormLogic from "~/lib/useFormLogic";
 
 type FormValue = {
   name: string;
+  email: string;
   age: number;
 };
 
 const PageForm: NextPage = () => {
-  const { current, updateForm } = useFormLogic<FormValue>({
+  const { current, updateForm, sections, valid } = useFormLogic<FormValue>({
+    plot: [
+      {
+        title: "お名前を教えて下さい",
+        required: true,
+        key: "name"
+      },
+      {
+        title: "年齢を教えて下さい",
+        required: false,
+        key: "age"
+      },
+      {
+        title: "メールアドレスを教えて下さい",
+        required: true,
+        key: "email"
+      }
+    ],
     defaultValue: {
       name: "",
+      email: "",
       age: 0
     }
   });
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (!valid) {
+      return;
+    }
+    console.log(current);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
+      {sections.map(({ plot }) => (
+        <div key={plot.key}>
+          <p>{plot.title}</p>
+          <p>
+            <input
+              type="text"
+              value={current[plot.key]}
+              onChange={e => {
+                switch (plot.key) {
+                  case "age":
+                    return updateForm(plot.key, Number(e.target.value));
+                  default:
+                    return updateForm(plot.key, e.target.value);
+                }
+              }}
+            />
+          </p>
+        </div>
+      ))}
       <p>
-        name:
-        <input
-          type="text"
-          value={current.name}
-          onChange={e => updateForm("name", e.target.value)}
-        />
-      </p>
-      <p>
-        age:
-        <input
-          type="number"
-          value={current.age}
-          onChange={e => updateForm("age", Number(e.target.value))}
-        />
+        <button type="submit" disabled={!valid}>
+          OK
+        </button>
       </p>
     </form>
   );
