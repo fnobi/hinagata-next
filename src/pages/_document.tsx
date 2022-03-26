edcdd6c6b1d2d6f5f5f81a96a39f4b30a7b39227
fnobi <fnobi0.8@gmail.com>
2022-03-26 09:03:45 +0900

    空の_documnet用意

---
+++ src/pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import { FC } from "react";
+
+const Document: FC = () => (
+  <Html>
+    <Head />
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default Document;
