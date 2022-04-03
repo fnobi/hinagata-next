2a095357cb2dfa896daa66180a3c9d10ac741ba3
Fujisawa Shin <fnobi0.8@gmail.com>
2022-04-03 21:44:03 +0900

    すこしutil追加

--- src/lib/cssUtil.ts
+++ src/lib/cssUtil.ts
@@ -12,3 +12,13 @@ export const percent = (...nums: CSSSizeKeyword[]) => appendPostfix(nums, "%");
 export const em = (...nums: CSSSizeKeyword[]) => appendPostfix(nums, "em");
 
 export const vw = (...nums: CSSSizeKeyword[]) => appendPostfix(nums, "vw");
+
+export const vh = (...nums: CSSSizeKeyword[]) => appendPostfix(nums, "vh");
+
+export const buildLinearGradient = (
+  turn: number,
+  ...pairs: [string, number][]
+) =>
+  `linear-gradient(${turn}deg, ${pairs
+    .map(([p1, p2]) => `${p1} ${percent(p2)}`)
+    .join(",")})`;
