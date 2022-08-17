d9477bf9c78d184a29646a8cf0786b55c3818233
fnobi <fnobi0.8@gmail.com>
2022-08-17 22:16:37 +0900

    さすがにdomをつかうのはナンセンスやんな

--- src/components/EasingScene.tsx
+++ src/components/EasingScene.tsx
@@ -26,37 +26,7 @@ const meterRootStyle = css({
   position: "relative",
   border: `solid ${px(1)} #000`,
   backgroundColor: "#cccccc",
-  overflow: "hidden",
-  canvas: { display: "block", position: "relative" }
-});
-
-const meterItemStyle = css({
-  position: "absolute",
-  left: percent(200),
-  top: percent(200),
-  width: em(1),
-  height: em(1),
-  marginLeft: em(-0.5),
-  marginTop: em(-0.5),
-  backgroundColor: "red",
-  borderRadius: percent(50)
-});
-
-const meterAxisStyle = css({
-  position: "absolute",
-  backgroundColor: "#fff"
-});
-const meterXAxisStyle = css(meterAxisStyle, {
-  left: percent(200),
-  top: percent(0),
-  height: percent(100),
-  width: px(1)
-});
-const meterYAxisStyle = css(meterAxisStyle, {
-  left: percent(0),
-  top: percent(200),
-  height: px(1),
-  width: percent(100)
+  canvas: { display: "block" }
 });
 
 const EasingScene: FC = () => {
@@ -64,64 +34,62 @@ const EasingScene: FC = () => {
   const [factor, setFactor] = useState(2);
   const [animInterval] = useState(2000);
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const meterItemRef = useRef<HTMLDivElement | null>(null);
-  const xAxisRef = useRef<HTMLDivElement | null>(null);
-  const yAxisRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     const { current: canvas } = canvasRef;
     if (!canvas) {
-      return;
+      return () => {};
     }
+
     const ctx = canvas.getContext("2d");
     if (!ctx) {
-      return;
+      return () => {};
     }
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     const fn = ELIST[name];
     if (!fn) {
-      return;
-    }
-
-    ctx.save();
-    ctx.translate(0, canvas.height);
-    ctx.scale(1, -1);
-    ctx.beginPath();
-    for (let i = 0; i <= canvas.width; i += 1) {
-      const y = fn(factor)(i / canvas.width) * canvas.height;
-      if (i) {
-        ctx.lineTo(i, y);
-      } else {
-        ctx.moveTo(i, y);
-      }
-    }
-    ctx.stroke();
-    ctx.restore();
-  }, [name, factor]);
-
-  useEffect(() => {
-    const { current: meterItem } = meterItemRef;
-    const { current: xAxis } = xAxisRef;
-    const { current: yAxis } = yAxisRef;
-    if (!meterItem || !xAxis || !yAxis) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
       return () => {};
     }
-    const fn = ELIST[name];
-    if (!fn) {
-      meterItem.style.top = "";
-      meterItem.style.left = "";
-      xAxis.style.left = "";
-      yAxis.style.top = "";
-      return () => {};
-    }
+
     return takeLoop(t => {
-      const v = (t % animInterval) / animInterval;
-      const y = fn(factor)(v);
-      meterItem.style.left = percent(v * 100);
-      xAxis.style.left = percent(v * 100);
-      meterItem.style.top = percent((1 - y) * 100);
-      yAxis.style.top = percent((1 - y) * 100);
+      const progress = (t % animInterval) / animInterval;
+      const value = fn(factor)(progress);
+      const animX = progress * canvas.width;
+      const animY = value * canvas.height;
+
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.save();
+      ctx.translate(0, canvas.height);
+      ctx.scale(1, -1);
+
+      ctx.strokeStyle = "#fff";
+      ctx.beginPath();
+      ctx.moveTo(animX, 0);
+      ctx.lineTo(animX, canvas.height);
+      ctx.moveTo(0, animY);
+      ctx.lineTo(canvas.width, animY);
+      ctx.stroke();
+
+      ctx.strokeStyle = "#000";
+      ctx.beginPath();
+      for (let i = 0; i <= canvas.width; i += 1) {
+        const y = fn(factor)(i / canvas.width) * canvas.height;
+        if (i) {
+          ctx.lineTo(i, y);
+        } else {
+          ctx.moveTo(i, y);
+        }
+      }
+      ctx.stroke();
+
+      ctx.fillStyle = "#f00";
+      ctx.beginPath();
+      ctx.moveTo(animX, animY);
+      ctx.arc(animX, animY, 5, 0, Math.PI * 2);
+      ctx.fill();
+
+      ctx.restore();
     });
   }, [name, factor]);
 
@@ -129,10 +97,7 @@ const EasingScene: FC = () => {
     <div css={wrapperStyle}>
       <div>
         <div css={meterRootStyle}>
-          <p ref={xAxisRef} css={meterXAxisStyle} />
-          <p ref={yAxisRef} css={meterYAxisStyle} />
           <canvas ref={canvasRef} width={500} height={500} />
-          <p ref={meterItemRef} css={meterItemStyle} />
         </div>
         <p>
           <select value={name} onChange={e => setName(e.target.value)}>
