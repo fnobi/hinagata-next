7cdb169c78dc26de0cde56efba5b9c3738a4edf0
fnobi <fnobi0.8@gmail.com>
2026-05-02 11:14:14 +0900

    emotion provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { type ReactNode } from "react";
+import EmotionProvider from "~/common/components/EmotionProvider";
 import { defaultMetadata } from "~/features/lib/defaultMetadata";
 import GlobalStyles from "~/features/components/GlobalStyles";
 import LayoutRoot from "~/features/components/LayoutRoot";
@@ -8,8 +9,10 @@ export const metadata = defaultMetadata;
 const RootLayout = ({ children }: { children: ReactNode }) => (
   <html lang="ja">
     <body>
-      <GlobalStyles />
-      <LayoutRoot>{children}</LayoutRoot>
+      <EmotionProvider>
+        <GlobalStyles />
+        <LayoutRoot>{children}</LayoutRoot>
+      </EmotionProvider>
     </body>
   </html>
 );
---
+++ src/common/components/EmotionProvider.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useState } from "react";
+import { useServerInsertedHTML } from "next/navigation";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import { type ReactNode } from "react";
+
+const EmotionProvider = ({ children }: { children: ReactNode }) => {
+  const [cache] = useState(() => {
+    const c = createCache({ key: "css" });
+    c.compat = true;
+    return c;
+  });
+
+  useServerInsertedHTML(() => (
+    <style
+      data-emotion={`${cache.key} ${Object.keys(cache.inserted).join(" ")}`}
+      dangerouslySetInnerHTML={{
+        __html: Object.values(cache.inserted).join(" ")
+      }}
+    />
+  ));
+
+  return <CacheProvider value={cache}>{children}</CacheProvider>;
+};
+
+export default EmotionProvider;
