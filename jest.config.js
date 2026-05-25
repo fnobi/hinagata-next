edb24e7c364e20376ac50bdf18d6feba3178e55e
fnobi <fnobi0.8@gmail.com>
2026-05-25 09:52:18 +0900

    ルートでも @hinagata が効くようにする

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,8 @@ module.exports = {
   testEnvironment: "node",
   roots: ["<rootDir>/src"],
   moduleNameMapper: {
-    "^~/(.*)$": "<rootDir>/src/$1"
+    "^~/(.*)$": "<rootDir>/src/$1",
+    "^@hinagata/core/(.*)$": "<rootDir>/src/$1"
   },
   transform: {
     "^.+\\.tsx?$": [
